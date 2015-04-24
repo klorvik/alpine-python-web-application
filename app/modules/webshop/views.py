cb8f6fe1653114bf273f4286e6ceb2a0301f5351
Knut Lorvik <lorviken@gmail.com>
2015-04-24 14:49:23 +0200

    Restructure webshop

--- app/modules/webshop/views.py
+++ app/modules/webshop/views.py
@@ -7,9 +7,24 @@ from app.modules.login.decorators import requires_login
 mod = Blueprint('webshop', __name__, url_prefix='/webshop')
 
 @mod.route('/')
-@requires_login
 def home():
-  return render_template("webshop/home.html", user=g.user)
+  return render_template("webshop/tickets.html", user=g.user)
+
+@mod.route('/tickets')
+def tickets():
+  return render_template("webshop/tickets.html", user=g.user)
+
+@mod.route('/tickets/<int:id>')
+def shop_tickets_item(id):
+  return render_template("webshop/ticket-item/%d.html" % id, user=g.user)
+
+@mod.route('/equipment')
+def equipment():
+  return render_template("webshop/equipment.html", user=g.user)
+
+@mod.route('/equipment/<int:id>')
+def shop_equipment_item(id):
+  return render_template("webshop/equipment-item/%d.html" % id, user=g.user)
 
 @mod.before_request
 def before_request():
